e13fbf763b2b65ca59a62e4e348a8ef2ac7e5769
bigbossbeastly <kevincpeterson10@gmail.com>
2018-03-07 21:41:50 -0800

    Initial setup

---
+++ Algs_PSet_04/src/Board.java
@@ -0,0 +1,63 @@
+
+public class Board
+{
+    int[][] board;
+    
+    public Board(int[][] blocks)
+    {
+        board = new int[blocks.length][blocks.length];
+        
+        for (int i = 0; i < blocks.length; i ++)
+        {
+            for (int j = 0; j < blocks[i].length; j++)
+            {
+                // 
+            }
+        }
+    }
+    
+    public int dimension()
+    {
+        
+    }
+    
+    public int hamming()
+    {
+        
+    }
+    
+    public int manhattan()
+    {
+        
+    }
+    
+    public boolean isGoal()
+    {
+        
+    }
+    
+    public Board twin()
+    {
+        
+    }
+    
+    public boolean equals(Object y)
+    {
+        
+    }
+    
+    public Iterable<Board> neighbors()
+    {
+        
+    }
+    
+    public String toString()
+    {
+        
+    }
+    
+    public static void main(String[] args)
+    {
+        
+    }
+}
